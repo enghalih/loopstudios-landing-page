36c03b911d34d7d11ca5c566df9632e023a9e54a
ghalih <enghalih685@gmail.com>
2026-02-05 20:56:17 +0700

    after revision

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const button = document.getElementById("menu-button");
 const menu = document.getElementById("menu");
 
-button?.addEventListener("click", () => {
-  const isActive = menu.getAttribute("aria-expanded");
+button?.addEventListener("click", (e) => {
+  const isActive = e.currentTarget.getAttribute("aria-expanded");
   if (isActive === "true") {
-    menu.setAttribute("aria-expanded", false);
+    e.currentTarget.setAttribute("aria-expanded", "false");
   } else {
-    menu.setAttribute("aria-expanded", true);
+    e.currentTarget.setAttribute("aria-expanded", "true");
   }
 });
